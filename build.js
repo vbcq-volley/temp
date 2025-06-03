263d9f5af3d964babb40fa5b1fdc947e0a2bbbee
alphaleadership <thomas.iniguez@free.fr>
2025-06-03 13:28:16 +0200

    sync

---
+++ build.js
@@ -0,0 +1,21 @@
+const esbuild = require('esbuild');
+
+async function build() {
+  try {
+    await esbuild.build({
+      entryPoints: ['./index.js'],
+      bundle: true,
+      minify: true,
+      outfile: 'bundle.js',
+      platform: 'node',
+      external: ['*.map', './node_modules/hexo'],
+     
+    });
+    console.log('Build terminé avec succès');
+  } catch (err) {
+    console.error('Erreur lors du build:', err);
+    process.exit(1);
+  }
+}
+
+build();
