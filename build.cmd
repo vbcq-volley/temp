547e76410d092a917a82cf9574f0a1dbada4a420
alphaleadership <thomas.iniguez@free.fr>
2025-06-12 14:47:12 +0200

    Mise à jour automatique des fichiers

--- build.cmd
+++ build.cmd
@@ -28,3 +28,4 @@ npx esbuild ./dist/hexo --bundle --platform=node --outfile=../../dist/hexo-cli.j
 npx esbuild ./lib/front_matter --bundle --platform=node --outfile=../../dist/hexo-front-matter.js
 npx esbuild ./lib/fs --bundle --platform=node --outfile=../../dist/hexo-fs.js
 npx esbuild ./index --bundle --platform=node --outfile=../../dist/hexo-generator-archive.js
+npx esbuild ./index --bundle --platform=node --outfile=../../dist/hexo-generator-category.js
