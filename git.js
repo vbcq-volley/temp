97ca4c3a5c9aefd95101e14b11f7970167bf28a4
alphaleadership <thomas.iniguez@free.fr>
2025-06-12 10:08:50 +0200

    Mise à jour automatique des fichiers

---
+++ git.js
@@ -0,0 +1,8 @@
+const process=require("child_process")
+const run=(cmd)=>{
+	const env=process.spawnSync(cmd,{
+		shell:true
+	})
+	//console.log(env.output[1].toString())
+	return env.output[1].toString()
+}
