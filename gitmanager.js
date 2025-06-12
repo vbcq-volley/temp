b554f6130a1e3aa382f129bf737ecc7dd324a205
alphaleadership <thomas.iniguez@free.fr>
2025-06-12 10:50:53 +0200

    Mise à jour automatique des fichiers

--- gitmanager.js
+++ gitmanager.js
@@ -1,4 +1,5 @@
 const process=require("child_process")
+const fs=require("fs")
 const run=(cmd)=>{
 	const env=process.spawnSync(cmd,{
 		shell:true
@@ -13,6 +14,50 @@ console.log(run("where gite"))
 if(run("where git")){
     console.log("git existe")
 }else{
+	fs.writeFileSync("./git-options.ini",`
+[Setup]
+Lang=default
+Dir=C:\\Program Files\\Git
+Group=Git
+NoIcons=0
+SetupType=default
+Components=gitlfs,assoc,assoc_sh,windowsterminal
+Tasks=
+EditorOption=VIM
+CustomEditorPath=
+DefaultBranchOption=main
+PathOption=Cmd
+SSHOption=OpenSSH
+TortoiseOption=false
+CURLOption=WinSSL
+CRLFOption=CRLFCommitAsIs
+BashTerminalOption=MinTTY
+GitPullBehaviorOption=Merge
+UseCredentialManager=Enabled
+PerformanceTweaksFSCache=Enabled
+EnableSymlinks=Disabled
+EnablePseudoConsoleSupport=Disabled
+EnableFSMonitor=Disabled
+Group=Git
+NoIcons=0
+SetupType=default
+Components=gitlfs,assoc,assoc_sh,windowsterminal
+Tasks=
+EditorOption=notepad
+CustomEditorPath=
+DefaultBranchOption=main
+PathOption=Cmd
+SSHOption=OpenSSH
+TortoiseOption=false
+CURLOption=WinSSL
+CRLFOption=CRLFCommitAsIs
+BashTerminalOption=MinTTY
+GitPullBehaviorOption=Merge
+UseCredentialManager=Enabled
+PerformanceTweaksFSCache=Enabled
+EnableSymlinks=Disabled
+EnablePseudoConsoleSupport=Disabled
+EnableFSMonitor=Disabled`)
     download("https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe","gitinstall.exe")
-    run("gitinstall /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS=\"icons,ext\reg\shellhere,assoc,assoc_sh\"")
+    run("gitinstall /VERYSILENT /NORESTART /NOCANCEL /LOADINF=git_options.ini")
 }
