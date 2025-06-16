59744b0e8cd8b42d16456987433a1789522dd740
alphaleadership <thomas.iniguez@free.fr>
2025-06-16 14:30:23 +0200

    Mise à jour automatique des fichiers

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { Octokit } = require("@octokit/rest");
 const t = require("git-credential-node")
 const login = t.fillSync("https://github.com")
 const fs = require('fs');
-
+t.
 // Initialisation du client Octokit
 const octokit = new Octokit({
   auth: login.password
