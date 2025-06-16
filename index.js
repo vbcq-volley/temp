8fd28c242f6b8760707e16c50c52ca3fed0cccc0
alphaleadership <thomas.iniguez@free.fr>
2025-06-16 13:35:42 +0200

    Mise à jour automatique des fichiers

--- index.js
+++ index.js
@@ -702,4 +702,5 @@ function lancerDansNouvelleFenetre(programme) {
         logger.error(`Erreur lors de la vérification des mises à jour : ${error.message}`);
     }
 }
-setInterval(60*1000,checkForUpdates(
+setInterval(60*1000,checkForUpdates())
+
