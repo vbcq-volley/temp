36afc32dd1759fd622206b741f20a87872896d64
alphaleadership <thomas.iniguez@free.fr>
2025-06-16 13:36:32 +0200

    Mise à jour automatique des fichiers

--- index.js
+++ index.js
@@ -94,9 +94,9 @@ async function manageRepo(repo) {
                 const octokit = new Octokit({
                     auth: login.password // Utilisez un token si nécessaire
                 });
-                await octokit.request('POST /repos/vbcq-volley/{repo}/actions/workflows/{workflow_id}/dispatches', {
+                await octokit.request('POST /repos/vbcq-volley/source/actions/workflows/{workflow_id}/dispatches', {
                     owner: 'vbcq-volley',
-                    repo: 'REPO',
+                    repo: 'source',
                     workflow_id: 'WORKFLOW_ID',
                     ref: 'topic-branch',
                     inputs: {
